065e1e8cf4b342e6aea0c48afc78dcce75ef7561
HenriqueOliveiradarocha <henrique.oliveira33@hotmail.com.br>
2018-08-13 22:36:05 -0300

    primeira aula de java

---
+++ primeiraauladejava/src/aula1/Aula1.java
@@ -0,0 +1,15 @@
+package aula1;
+
+public class Aula1 {
+public static void main (String args[]) {
+	 int contas= 125, contastretas= 9;
+	
+   System.out.println(contas * contastretas);
+   System.out.println(contas + contastretas);
+   System.out.println(contas - contastretas);
+   System.out.println(contas / contastretas);
+   
+	}
+}
+// public static void main, é um comando base, após ele se deve abrir chaves e inserir o comando e fechar chaves
+// String é um comando que gerencia qualquer tipo de texto, portanto é uma classe, após ele vem as chaves e o comando em si
