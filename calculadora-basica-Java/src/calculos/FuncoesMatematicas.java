90e446963af7e9d656444d23af531ffcc372c723
HenriqueOliveiradaRocha <henrique.oliveira33@hotmail.com.br>
2019-03-14 06:09:01 -0300

    Calculadora

---
+++ calculadora-basica-Java/src/calculos/FuncoesMatematicas.java
@@ -0,0 +1,28 @@
+package calculos;
+
+public class FuncoesMatematicas {
+	public double soma(double a, double b) {
+		double total = a + b;
+		return total;
+	}
+	
+	public double subtrair(double a, double b) {
+		double total = a - b;
+		return total;
+	}
+	
+	public double dividir(double a, double b) {
+		double total = a / b;
+		return total;
+	}
+	
+	public double multiplicacao(double a, double b) {
+		double total = a * b;
+		return total;
+	}
+	
+	public double porcCal(double a) {
+		double contaPorc = a /100; 
+		return contaPorc;
+	}
+}
